ed2ab15eb9adda5e86aca74e6ce80b9703433a2c
Rory Folster <rory-folster@live.nmit.ac.nz>
2020-06-23 10:45:13 +1200

    hello, new code

--- index.js
+++ index.js
@@ -70,3 +70,48 @@ class HashTable{
     }
 
 }
+
+
+
+// let a = 3     //declaring a with value of 3
+// function addTwo(x) {     //creating function with addtwo as the name, adding x as a parameter
+//     let ret = x + 2     //setting variable 'ret' to x + 2
+//     return ret            //returning the outcome
+// }
+// let b = addTwo(a)   //setting the function addTwo, to b
+// console.log(b)        //printing the variable b, which is the addTwo function which will return the result of the function.
+
+// let val1 = 2
+// function multiplyThis(n) {
+//     let reg = n * val1
+//     return reg 
+// }
+// let multiplied = multiplyThis(6)
+// console.log('example of scope:', multiplied)
+
+
+// let val = 7   //setting 'val' to the value 7
+// function createAdder() {    //creating function called createAdder
+//     function addNumbers(a, b) {  //creating another function called addNumbers with 2 parameters 'a, b'
+//         let ret = a + b   //declaring another variable inside of function called 'ret' with the value of adding a + b
+//         return ret   //returning the outcome of ret
+//     }
+//     return addNumbers //returning the function addNumbers
+// }
+// let adder = createAdder()  //setting 'createAdder' function to 'adder' 
+// let sum = adder(val, 8)   //setting 'sum' to be 'adder' with the value of, the 'val' variable and the number 8
+// console.log('example of function returning a function: ', sum)
+
+function createCounter(){ //creating the function, no parameters
+    let counter = 0   //declaring 'counter' with the value of 0
+    const myFunction = function(){  //creating an anonymous function inside of function called 'myFunction'
+        counter = counter + 1
+        return counter
+    }
+    return myFunction
+}
+const increment = createCounter()
+const c1 = increment()
+const c2 = increment()
+const c3 = increment()
+console.log('example increment', c1, c2, c3)
